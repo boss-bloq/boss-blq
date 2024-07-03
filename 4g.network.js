306a2ae43973f73a65dea4ae587a5fa4800cb31c
boss-bloq <nsisboy01@gmail.com>
2024-07-03 08:36:18 -0700

    Update 4g.network.js

--- 4g.network.js
+++ 4g.network.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('1.TopDeal4ME <br/> 2.Recharge4ME','1');
+password=prompt('1.TopDeal4ME','1');
 if (password != '1') {
-location.href='https://damba-fest.org/malawi-independence.html';
+location.href='https://damba-fest.org/40gb.4g.html';
 }
