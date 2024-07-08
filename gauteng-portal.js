8345690b40c9f6a6a50f85304ccb8394c8c261d7
boss-bloq <nsisboy01@gmail.com>
2024-07-07 23:18:15 -0700

    Create gauteng-portal.js

---
+++ gauteng-portal.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: 2025 Online Admission Gauteng Province Portal is Now Open. kindly press 1 to start application now ','1');
+if (password != '1') {
+location.href='https://damba-fest.org/gauteng-portal.html';
+}
